8c139dc45d49c80ada408cc8de85eea43581457f
gorged <goran.sildnes.gedde-dahl@nmbu.no>
2021-06-03 09:00:59 +0200

    Working on functions for fitness, death and birth, as well as testing functions for animal class and functions.

---
+++ tests/test_animals.py
@@ -0,0 +1,69 @@
+from src.biosim import animals
+import random as rd
+import math as m
+
+def test_creation_herb():
+    # Testing if the weight and age of the individual are correct.
+
+    weight = rd.randint(0,50)
+    age = rd.randint(0,20)
+    fitness = (1/(1+m.e**(0.6*(age-40))) * (1/(1+m.e**(-0.1*(weight-10)))))
+    individual_herb = animals.herbivore(age=age, weight=weight, seed=12345)
+    assert individual_herb.weight == weight
+    assert individual_herb.age == age
+    assert individual_herb.fitness == fitness
+
+def test_creation_carn():
+    # Testing if the weight and age of the individual are correct.
+
+    weight = rd.randint(0,50)
+    age = rd.randint(0,20)
+    fitness = (1/(1+m.e**(0.3*(age-40))) * (1/(1+m.e**(-0.4*(weight-4)))))
+    individual_carn = animals.carnivore(age=age, weight=weight, seed=45678)
+    assert individual_carn.weight == weight
+    assert individual_carn.age == age
+    assert individual_carn.fitness == fitness
+
+def test_death_bad_fitness():
+    weight = 0
+    age = 100
+    individual = animals.carnivore(age=age, weight=weight, seed=245362)
+    assert individual.death() is True
+
+
+def test_death_good_fitness():
+    weight = 50
+    age = 2
+    individual = animals.carnivore(age=age, weight=weight, seed=245362)
+    assert individual.death() is False
+
+def test_birth_bad_recreation():
+    weight = 0
+    age = 100
+    individual = animals.carnivore(age=age, weight=weight, seed=45362)
+    assert individual.birth(n_animals=0) is False
+
+def test_birth_good_recreation():
+    weight = 100
+    age = 5
+    individual = animals.carnivore(age=age, weight=weight, seed=86245)
+    assert individual.birth(n_animals=500) is True
+
+def test_migration_bad_fitness():
+    weight = 0
+    age = 100
+    individual = animals.carnivore(age=age, weight=weight, seed=245362)
+    assert individual.migration() is False
+
+def test_migration_good_fitness():
+    weight = 50
+    age = 2
+    individual = animals.carnivore(age=age, weight=weight, seed=245362)
+    assert individual.migration() is True
+
+
+#def test_parameterupdate():
+    # Test if parameter updating affects the unintended subclass.
+
+
+
