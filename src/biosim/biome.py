f66a4c4c50f2281ee681de78fb7acbf2eb899643
Ahmar Abbas <ahmar.abbas@nmbu.no>
2021-06-04 13:34:32 +0200

    added population, death, fooder update

--- src/biosim/biome.py
+++ src/biosim/biome.py
@@ -10,10 +10,30 @@ class biome:
         self.herb = []
         self.carn = []
 
-    def add_population(self,pop):
+    def update_fodder(self):
+        self.fodder = self.f_max
+
+    def add_population(self, pop):
         for specie in pop:
-            specie['specie']
-            self.herb.append()
+            if specie['species'] == 'Herbivore':
+                self.herb.append(herbivore(specie['age'], specie['weight']))
+            elif specie['species'] == 'Carnivore':
+                self.carn.append(herbivore(specie['age'], specie['weight']))
+
+    def remove_population(self):
+        self.herb[:] = [specie for specie in self.herb if not specie.death()]
+        self.carn[:] = [specie for specie in self.carn if not specie.death()]
+
+    def grazing(self):
+        pass
+
+    def breeding(self):
+        pass
+
+
+
+
+
 
 
 
@@ -43,3 +63,17 @@ class water(biome):
     def __init__(self, loc):
         self.f_max = 0
         super().__init__(loc)
+
+A = lowland((1,1))
+A.add_population([{'species': 'Herbivore',
+                           'age': 5,
+                           'weight': 20}])
+A.add_population([{'species': 'Herbivore',
+                           'age': 5,
+                           'weight': 20}])
+A.add_population([{'species': 'Herbivore',
+                           'age': 5,
+                           'weight': 20}])
+print(A.herb)
+A.remove_population()
+print(A.herb)
